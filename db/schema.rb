5718f43b31fea690adf6d177a892fc3a941871b3
dongmingjuan <mingjuandong@mingdeMacBook-Pro.local>
2017-10-19 12:02:55 +0800

    you can choose category from product form

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019035119) do
+ActiveRecord::Schema.define(version: 20171019035516) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
